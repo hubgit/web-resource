dad04a801a6adc06dcab9b4914151f5242e1e3c1
Alf Eaton <hubgit@@users.noreply.github.com>
2014-05-08 08:44:13 +0100

    Add pagination

--- resource.js
+++ resource.js
@@ -10,7 +10,15 @@ var Resource = function(url, params) {
     }
 };
 
-Resource.prototype.get = function(responseType, headers) {
+Resource.prototype.buildQueryString = function(params) {
+    var items = Object.keys(params).map(function(key) {
+        return encodeURIComponent(key) + '=' + encodeURIComponent(params[key]);
+    });
+
+    return items.length ? '?' + items.join('&').replace(/%20/g, '+') : '';
+};
+
+Resource.prototype.prepareHeaders = function(headers, responseType) {
     headers = headers || {};
 
     switch (responseType) {
@@ -20,28 +28,69 @@ Resource.prototype.get = function(responseType, headers) {
 
         case 'html':
             headers.accept = headers.accept || 'text/html';
-            responseType = 'document';
             break;
 
         case 'xml':
             headers.accept = headers.accept || 'application/xml';
+            break;
+    }
+
+    return headers;
+};
+
+Resource.prototype.prepareResponseType = function(responseType) {
+    switch (responseType) {
+        case 'html':
+        case 'xml':
             responseType = 'document';
             break;
     }
 
-    var request = new HTTP.Request({
+    return responseType;
+};
+
+Resource.prototype.get = function(responseType, headers) {
+    var options = {
         url: this.url,
-        headers: headers,
-        responseType: responseType,
-    });
+        headers: this.prepareHeaders(headers, responseType),
+        responseType: this.prepareResponseType(responseType),
+    };
+
+    if (this.pagination) {
+        return this.getPages(options, this.pagination);
+    }
+
+    var request = new HTTP.Request(options);
 
     return request.promise;
 };
 
-Resource.prototype.buildQueryString = function(params) {
-    var items = Object.keys(params).map(function(key) {
-        return encodeURIComponent(key) + '=' + encodeURIComponent(params[key]);
-    });
+//pagination: { items: function(page) { … }, next: function(page) { … } }
 
-    return items.length ? '?' + items.join('&').replace(/%20/g, '+') : '';
+Resource.prototype.getPages = function(options, pagination) {
+    return new Promise(function(resolve, reject) {
+        var items = [];
+
+        var fetch = function() {
+            var request = new HTTP.Request(options);
+
+            request.promise.then(function(page) {
+                pagination.items(page).forEach(function(item) {
+                    console.log(item);
+                    items.push(item);
+                });
+
+                options.url = pagination.next(page);
+
+                if (options.url) {
+                    fetch();
+                } else {
+                    resolve(items);
+                }
+            }, reject);
+        };
+
+        fetch();
+    });
 };
+
